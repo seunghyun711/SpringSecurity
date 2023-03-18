7a08874170d9fcfca1ded55b6e73d42a8d0cc33f
seunghyun711 <seunghyun711@gmail.com>
2023-03-18 22:25:52 +0900

    DBMemberService 구현

--- src/main/java/com/example/SpringSecurityPractice/member/DBMemberService.java
+++ src/main/java/com/example/SpringSecurityPractice/member/DBMemberService.java
@@ -1,11 +1,38 @@
 package com.example.SpringSecurityPractice.member;
 
+import com.example.SpringSecurityPractice.exception.BusinessLogicException;
+import com.example.SpringSecurityPractice.exception.ExceptionCode;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+// DB에 User를 등록하기 위한 클래스
 @Transactional
 public class DBMemberService implements MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // 1
+    public DBMemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public Member createMember(Member member) {
-        return null;
+        verifyExistsEmail(member.getEmail());
+        String encryptedPassword = passwordEncoder.encode(member.getPassword()); // 패스워드 암호화
+        member.setPassword(encryptedPassword); // 암호화된 패스워드를 password필드에 다시 할당
+
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+        }
     }
 }
