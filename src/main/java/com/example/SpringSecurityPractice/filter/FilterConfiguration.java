50196795ad3905237d948b589d81a125839e45af
seunghyun711 <seunghyun711@gmail.com>
2023-03-20 00:43:25 +0900

    두 번째 Filter 등록 & Filter 순서 설정

--- src/main/java/com/example/SpringSecurityPractice/filter/FilterConfiguration.java
+++ src/main/java/com/example/SpringSecurityPractice/filter/FilterConfiguration.java
@@ -12,6 +12,14 @@ public class FilterConfiguration {
     @Bean
     public FilterRegistrationBean<FirstFilter> firstFilterRegister(){
         FilterRegistrationBean<FirstFilter> registrationBean = new FilterRegistrationBean<>(new FirstFilter());
+        registrationBean.setOrder(1); // Filter가 지정된 순서대로 진행되도록 순서 지정
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<SecondFilter> secondFilterRegister(){
+        FilterRegistrationBean<SecondFilter> registrationBean = new FilterRegistrationBean<>(new SecondFilter());
+        registrationBean.setOrder(2); // Filter가 지정된 순서대로 진행되도록 순서 지정
         return registrationBean;
     }
 }
